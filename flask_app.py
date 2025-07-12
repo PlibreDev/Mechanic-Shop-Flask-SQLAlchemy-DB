21b7b964eb706a9bb210f7bc47c2ecffc0d140c8
PlibreDev <p.jaghab@gmail.com>
2025-07-11 23:27:36 -0400

    more debugging

--- flask_app.py
+++ flask_app.py
@@ -3,8 +3,3 @@ from app.extensions import db
 
 app = create_app('ProductionConfig')
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    print(app.url_map)
-    app.run()
