d0f748e0a6dfe306ec65978fb4e78b861559e3b6
PlibreDev <p.jaghab@gmail.com>
2025-07-12 19:08:04 -0400

    testing build

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,8 @@
 from app import create_app
-from app.extensions import db
+from app.models import db
 
 app = create_app('ProductionConfig')
 
+with app.app_context():
+    # db.drop_all()
+    db.create_all()
